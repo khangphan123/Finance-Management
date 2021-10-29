d1916a6c9fa190c816c35c227e42349b1edeeb90
khangphan123 <61067450+khangphan123@users.noreply.github.com>
2021-10-28 20:53:20 -0700

    New Update

--- src/main/model/Customer.java
+++ src/main/model/Customer.java
@@ -26,6 +26,7 @@ public class Customer implements Writeable {
         this.interestRate = 0.05;
         this.stockPortfolios = new ArrayList<>();
     }
+
     public void setBalance(double amount) {
         balance = amount;
     }
